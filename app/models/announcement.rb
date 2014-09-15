3872ceece8eb596a3393d6406fe727664ed4e09f
chrismayu <chrismayu@gmail.com>
2014-09-15 16:11:00 -0400

    annoucment model updated

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -1,2 +1,18 @@
 class Announcement < ActiveRecord::Base
+
+  scope :current, -> { where("starts_at <= :now and ends_at >= :now", now: Time.zone.now) }
+  
+  validates :message, presence: true, length: { maximum: 200 }
+  
+  validates_datetime :starts_at
+  validates_datetime :ends_at, :after => :starts_at, :after_message => 'must be before "Start Time"'
+  
+  
+  
+  def self.current(hidden_ids = nil)
+    result = where("starts_at <= :now and ends_at >= :now", now: Time.zone.now)
+    result = result.where("id not in (?)", hidden_ids) if hidden_ids.present?
+    result
+  end
+
 end
