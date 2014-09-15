73b4f28a91bb97270b30ccd16774890550a8c45a
chrismayu <chrismayu@gmail.com>
2014-09-15 17:30:53 -0400

    added speakers model

--- app/models/service_time.rb
+++ app/models/service_time.rb
@@ -1,2 +1,6 @@
 class ServiceTime < ActiveRecord::Base
+  validates :service_name, presence: true 
+  
+   default_scope order: 'service_times.day ASC'
 end
+
--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -1,2 +1,21 @@
 class Speaker < ActiveRecord::Base
+  has_many :speaker_pictures, :foreign_key => :speaker_id, :primary_key =>  :id, :inverse_of => :speaker, :dependent => :destroy
+  has_many :sermons, :foreign_key => :speaker_id, :primary_key =>  :id, :inverse_of => :speaker 
+  
+  
+  validates  :first_name,  :presence => {:message => 'Must enter a first name'}
+  validates  :last_name,  :presence => {:message => 'Must enter a last name'}
+  validates :description, presence: true, length: { maximum: 140 }
+ 
+  
+  
+  before_save :combine_first_and_last_name
+ 
+  private 
+
+  def combine_first_and_last_name
+     full_name = "#{self.first_name} #{self.last_name}" 
+     self.full_name = full_name
+  end
+
 end
