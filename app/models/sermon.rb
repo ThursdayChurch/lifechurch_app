7a5257c0a5495f7eab1724d15bd5e367105d7f4f
chrismayu <chrismayu@gmail.com>
2014-09-15 17:29:51 -0400

    added sermons model

--- app/models/sermon.rb
+++ app/models/sermon.rb
@@ -1,2 +1,59 @@
 class Sermon < ActiveRecord::Base
+  
+    include ActionView::Helpers::TextHelper
+  extend FriendlyId
+ friendly_id :slug_candidates, use: :slugged 
+  
+  resourcify
+
+belongs_to :speaker
+has_many :sermon_pictures, :foreign_key => :sermons_id, :primary_key =>  :id, :inverse_of => :sermon, :dependent => :destroy
+
+
+validates  :title, :presence => true
+validates  :date_of_sermon, :presence => true
+validates  :category, :presence => true
+ 
+
+after_save :create_description
+
+#default_scope order: 'sermons.date_of_sermon DESC'
+
+
+after_validation :move_friendly_id_error_to_name
+
+  def move_friendly_id_error_to_name
+    errors.add :title, *errors.delete(:friendly_id) if errors[:friendly_id].present?
+  end
+  def slug_candidates
+    [
+      :title#,
+    #  [:name, :history]#,
+     # [:name, :street, :city],
+     # [:name, :street_number, :street, :city]
+    ]
+  end
+ 
+def self.search(search)
+  if search
+    find(:all, :conditions => ['description LIKE ?', "%#{search}%"])
+  else
+    find(:all)
+  end
+end
+
+private
+
+   def create_description
+       sermon= Sermon.find(id)
+       speaker = sermon.speaker 
+       keyword = "#{speaker.title} #{speaker.full_name} #{sermon.date_of_sermon.strftime("%B, %b, %m, %A, %a, %d, %Y") } #{sermon.category}  #{sermon.title} #{sermon.date_of_sermon.strftime("%B #{sermon.date_of_sermon.day.ordinalize}, %Y") }"
+       truncated_keyword =  truncate(keyword, :length => 250)    
+       sermon.update_column(:description, truncated_keyword )
+   end
+ 
+  
+ 
+end
+  
 end
--- app/models/sermons_picture.rb
+++ app/models/sermons_picture.rb
@@ -1,2 +1,28 @@
 class SermonsPicture < ActiveRecord::Base
+ # mount_uploader :sermon_image, SermonImageUploader
+  belongs_to :sermon, :inverse_of => :sermon_pictures
+
+
+ 
+     after_create :enqueue_image
+
+     def image_name
+       File.basename(sermon_image.path || sermon_image.filename) if sermon_image
+     end
+
+     def enqueue_image
+        perform(id, key) if key.present?
+     end
+
+
+
+       def perform(id, key)
+         sermonpicture = SermonsPicture.find(id)
+         sermonpicture.key = key
+         sermonpicture.remote_sermon_image_url = sermonpicture.sermon_image.direct_fog_url(with_path: true)
+         sermonpicture.save!
+         sermonpicture.update_column(:image_processed, true)
+       end
 end
+  
+ 
