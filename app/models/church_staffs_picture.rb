0405239807dd81c13f38a0bfb8abfaef3077588c
chrismayu <chrismayu@gmail.com>
2014-09-15 16:14:25 -0400

    staff pictures model

--- app/models/church_staffs_picture.rb
+++ app/models/church_staffs_picture.rb
@@ -1,2 +1,33 @@
 class ChurchStaffsPicture < ActiveRecord::Base
+  
+ # mount_uploader :staff_image, StaffImageUploader
+
+  belongs_to :church_staff, :inverse_of => :church_staffs_pictures
+
+
+  validates :church_staff_id, :uniqueness => {:message => 'This Staff Already has a Picture Setup - Delete other Picture first'}
+
+   after_create :enqueue_image
+
+   def image_name
+     File.basename(staff_image.path || staff_image.filename) if staff_image
+   end
+
+   def enqueue_image
+      perform(id, key) if key.present?
+   end
+
+   
+
+     def perform(id, key)
+       churchstaffpicture = ChurchStaffsPicture.find(id)
+       churchstaffpicture.key = key
+       churchstaffpicture.remote_staff_image_url = churchstaffpicture.staff_image.direct_fog_url(with_path: true)
+       churchstaffpicture.save!
+       churchstaffpicture.update_column(:image_processed, true)
+     end
+     
+     
+     
+     
 end
