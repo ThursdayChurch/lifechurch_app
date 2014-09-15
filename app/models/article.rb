3e7d9a413d42c163e7a76f60afde3fafbe217594
chrismayu <chrismayu@gmail.com>
2014-09-15 16:12:24 -0400

    article modal updated

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,24 @@
 class Article < ActiveRecord::Base
+  extend FriendlyId
+  friendly_id :slug_candidates, use: :slugged 
+  
+  
+  after_validation :move_friendly_id_error_to_name
+
+    def move_friendly_id_error_to_name
+      errors.add :name, *errors.delete(:friendly_id) if errors[:friendly_id].present?
+    end
+   
+  
+  
+  
+  def slug_candidates
+    [
+      :name#,
+    #  [:name, :history]#,
+     # [:name, :street, :city],
+     # [:name, :street_number, :street, :city]
+    ]
+  end
+ 
 end
