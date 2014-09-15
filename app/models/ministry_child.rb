514205c1cc25724b4bce3466af2ea4234889071b
chrismayu <chrismayu@gmail.com>
2014-09-15 17:27:22 -0400

    added Ministries model

--- app/models/ministry_child.rb
+++ app/models/ministry_child.rb
@@ -1,2 +1,38 @@
 class MinistryChild < ActiveRecord::Base
+  extend FriendlyId
+    friendly_id :slug_candidates, use: :slugged 
+  
+  
+  attr_accessible :advert_main_page, :contact_email, :ministry_id, :contact_person, :description, :display_rank, :name, :ta1, :ta2, :ta3, :ta4, :ta5
+
+  belongs_to :ministry, :inverse_of => :ministry_children
+  
+  has_many :ministry_picture_children, :foreign_key => :ministry_id, :primary_key =>  :id, :inverse_of => :ministry_child, :dependent => :destroy
+   
+  validates :description, presence: true, length: { maximum: 140 }
+
+ validates :display_rank, presence: true
+ 
+  # validates_format_of :contact_email, :with => /.+@.+\..+/i
+
+
+
+ 
+  default_scope order: 'ministry_children.display_rank ASC' 
+ 
+  after_validation :move_friendly_id_error_to_name
+
+    def move_friendly_id_error_to_name
+      errors.add :name, *errors.delete(:friendly_id) if errors[:friendly_id].present?
+    end
+    def slug_candidates
+      [
+        :name#,
+      #  [:name, :history]#,
+       # [:name, :street, :city],
+       # [:name, :street_number, :street, :city]
+      ]
+    end
+ 
+
 end
--- app/models/ministry_children_picture.rb
+++ app/models/ministry_children_picture.rb
@@ -1,2 +1,29 @@
 class MinistryChildrenPicture < ActiveRecord::Base
+ # mount_uploader :ministrychild_image, MinistrychildImageUploader
+  belongs_to :ministry_child, :inverse_of => :ministry_picture_children
+ 
+     after_create :enqueue_image
+
+     def image_name
+       File.basename(ministrychild_image.path || ministrychild_image.filename) if ministrychild_image
+     end
+
+     def enqueue_image
+        perform(id, key) if key.present?
+     end
+
+
+
+       def perform(id, key)
+         ministrychildpicture = MinistryChildrenPicture.find(id)
+         ministrychildpicture.key = key
+         ministrychildpicture.remote_ministrychild_image_url = ministrychildpicture.ministrychild_image.direct_fog_url(with_path: true)
+         ministrychildpicture.save!
+         ministrychildpicture.update_column(:image_processed, true)
+       end
+  
+  
 end
+  
+  
+ 
