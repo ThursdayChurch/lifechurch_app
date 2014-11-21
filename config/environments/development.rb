d7ecb981b97c793a04053878e8f1918cb3b39602
chrismayu <chrismayu@gmail.com>
2014-11-20 20:22:14 -0500

    Added quick asset fixed navbar js

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-
+ 
   # Do not eager load code on boot.
   config.eager_load = false
 
@@ -25,9 +25,9 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  config.assets.debug = false
   
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   config.quiet_assets = true
   
